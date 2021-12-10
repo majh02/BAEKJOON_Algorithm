1d19a56a515123d2a38d8059e3b002a3f7ab6cb5
MAJIHYE <majh0294@gmail.com>
2021-12-10 18:27:04 +0900

    [2021-12-10]

--- 그리디알고리즘/그리디알고리즘_1931.cpp
+++ 그리디알고리즘/그리디알고리즘_1931.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main(){
     int N;
     int meeting[MAX][2]={0,};
-    int count=1;
     cin>>N;
     if(N<1||N>MAX) return 0;
     for(int i=0;i<N;i++){
@@ -18,7 +17,7 @@ int main(){
 
     for(int i=0;i<N;i++){
         for(int j=i+1;j<N;j++){
-            if(meeting[i][0]>meeting[j][0]){
+            if(meeting[i][1]>meeting[j][1]){
                 int tmp0 = meeting[i][0];
                 int tmp1 = meeting[i][1];
                 meeting[i][0] = meeting[j][0];
@@ -26,33 +25,24 @@ int main(){
                 meeting[j][0] = tmp0;
                 meeting[j][1] = tmp1;
             }
-            else if(meeting[i][0]==meeting[j][0]&&meeting[i][1]>meeting[j][1]){
-                int tmp = meeting[i][1];
-                meeting[i][1] = meeting[j][1];
-                meeting[j][1] = tmp;
+            else if(meeting[i][1]==meeting[j][1]&&meeting[i][0]>meeting[j][0]){
+                int tmp = meeting[i][0];
+                meeting[i][0] = meeting[j][0];
+                meeting[j][0] = tmp;
             }
         }
     }
 
-    int a=0, b=1;
-    int max_count=0;
+    int count=0;
+    int prev_end_time=0;
     for(int i=0;i<N;i++){
-        count=1;
-        a=i; b=i+1;
-        while(1){
-            if(meeting[a][1]<=meeting[b][0]){
-                count++;
-                cout<<"a: "<<a<<", b: "<<b<<", count: "<<count<<endl;
-                a=b;
-            }
-            b++;
-            if(b>=N) break;
+        if(prev_end_time<=meeting[i][0]){
+            prev_end_time=meeting[i][1];
+            count++;
         }
-        max_count=max(max_count,count);
-        cout<<endl;
     }
 
-    cout<<max_count<<endl;
+    cout<<count<<endl;
 
     return 0;
 }
