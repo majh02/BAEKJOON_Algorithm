5ebb306e97935a71c9c0aa137d1ecbbec4157a1b
MAJIHYE <majh0294@gmail.com>
2022-02-06 20:24:03 +0900

    [2022-02-06]

---
+++ DFS와BFS/DFS와BFS/DFS와BFS_2178.java
@@ -0,0 +1,6 @@
+//2178번_미로탐색
+package DFS와BFS;
+
+public class DFS와BFS_2178 {
+    
+}
--- DFS와BFS/DFS와BFS/DFS와BFS_2667.java
+++ DFS와BFS/DFS와BFS/DFS와BFS_2667.java
@@ -5,15 +5,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class DFS와BFS_2667 {
-    private static int[] dx = {0, 0, 1, -1};
-    private static int[] dy = {1, -1, 0, 0};
-    private static int N;
-    private static boolean[][] house = new boolean[26][26];
-    private static int[] complex; //단지 수
-    private static boolean[][] visited = new boolean[26][26];
-    private static int num = 0;
+    public static int[] dx = {0, 0, 1, -1};
+    public static int[] dy = {1, -1, 0, 0};
+    public static int N;
+    public static boolean[][] house = new boolean[26][26];
+    public static int[] complex; //단지 수
+    public static boolean[][] visited = new boolean[26][26];
+    public static int num = 0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
@@ -30,11 +32,12 @@ public class DFS와BFS_2667 {
         }
 
         visited = new boolean[N+1][N+1];
-        complex = new int[(N+1)*(N+1)];
+        complex = new int[N*N];
         for(int i=1;i<=N;i++){
             for(int j=1;j<=N;j++){
                 if(house[i][j] && !visited[i][j]){
-                    DFS(i,j);
+                    // DFS(i,j);
+                    BFS(i,j);
                     num++;
                 }
             }
@@ -48,7 +51,7 @@ public class DFS와BFS_2667 {
         }
     }
 
-    private static void DFS(int x, int y){
+    public static void DFS(int x, int y){
 
         visited[x][y] = true;
         complex[num]++;
@@ -59,6 +62,31 @@ public class DFS와BFS_2667 {
             if(nx>0 && ny>0 && nx<=N && ny<=N){
                 if(house[nx][ny] && !visited[nx][ny]){
                     DFS(nx,ny);
+                }
+            }
+        }
+    }
+
+    public static void BFS(int x, int y){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{x,y});
+        visited[x][y] = true;
+        complex[num]++;
+
+        while(!queue.isEmpty()){
+            int X = queue.peek()[0];
+            int Y = queue.peek()[1];
+            queue.poll();
+
+            for(int i=0;i<4;i++){
+                int nx = X+dx[i];
+                int ny = Y+dy[i];
+                if(nx > 0 && ny > 0 && nx<=N && ny<=N){
+                    if(house[nx][ny] && !visited[nx][ny]){
+                        queue.add(new int[]{nx,ny});
+                        visited[nx][ny] = true;
+                        complex[num]++;
+                    }
                 }
             }
         }
