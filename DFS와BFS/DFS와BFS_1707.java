57e0f29831b27f9e081f77cb8b39ed96aa505b70
MAJIHYE <majh0294@gmail.com>
2022-02-12 00:08:26 +0900

    [2022-02-12]

--- DFS와BFS/DFS와BFS_1707.java
+++ DFS와BFS/DFS와BFS_1707.java
@@ -8,9 +8,11 @@ public class DFS와BFS_1707 {
     public static int K, V, E;
     public static int[][] graph;
     public static int[] visited;
+    public static boolean flag;
     public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         K = Integer.parseInt(br.readLine());
         for(int k=0;k<K;k++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -26,13 +28,12 @@ public class DFS와BFS_1707 {
                 graph[x][y] = graph[y][x] = 1;
             }
 
-            boolean flag = false;
+            flag = false;
             for(int i=1;i<=V;i++){
-                for(int j=i+1;j<=V;j++){
+                for(int j=1;j<=V;j++){
                     if(graph[i][j]==1 && visited[i]==0){
-                        visited[i] = 1;
-                        int result = BFS(j);
-                        if(result==0) flag = true;
+                        BFS(i);
+                        // DFS(i,j);
                     }
                 }
                 if(flag==false) sb.append("NO\n");
@@ -43,27 +44,51 @@ public class DFS와BFS_1707 {
         System.out.println(sb);
     }
 
-    public static int count = 0;
-    public static int BFS(int x){
+    public static void DFS(int x, int y){
+
+        graph[x][y] = graph[y][x] = 0;
+        visited[y] = visited[x]*(-1);
+        
+        for(int i=1;i<=V;i++){
+            if(graph[y][i]==1 && visited[i]==0){
+                DFS(y,i);
+            }
+            else if(graph[y][i]==1 && visited[y]==visited[i]){
+                flag = false;
+                return;
+            }
+        }
+
+        flag = true;
+        return;
+    }
+
+    public static void BFS(int x){
         Queue<Integer> queue = new LinkedList<>();
         queue.add(x);
-        visited[x] = -1;
+        visited[x] = 1;
 
         while(!queue.isEmpty()){
             int tmp = queue.poll();
 
-            for(int i=tmp+1;i<=V;i++){
+            for(int i=1;i<=V;i++){
                 if(graph[tmp][i]==1 && visited[i]==0){
+                    graph[tmp][i] = graph[i][tmp] = 0;
                     visited[i] = visited[tmp]*(-1);
                     queue.add(i);
                 }
-                else if(visited[tmp]+visited[i]==0){
-                    return 1;
+                else if(graph[tmp][i]==1 && visited[tmp]+visited[i]==0){
+                    flag = false;
+                    return;
                 }
-                else return 1;
+                else if(graph[tmp][i]==1 && visited[tmp]==visited[i]){
+                    flag = false;
+                    return;
+                }
             }
         }
-        return 0;  
+        flag = true;
+        return;
     }
     
 }
