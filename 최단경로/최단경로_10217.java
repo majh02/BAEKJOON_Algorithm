b0e77c331c8c11ce7638f97a08afa74a96da6bd9
MAJIHYE <majh0294@gmail.com>
2022-02-23 00:21:35 +0900

    [2022-02-23]

--- 최단경로/최단경로_10217.java
+++ 최단경로/최단경로_10217.java
@@ -4,12 +4,19 @@ package 최단경로;
 import java.io.*;
 import java.util.*;
 
-class Airport{
-    int end, cost, time;
+class Airport implements Comparable<Airport>{
+    int end;
+    int cost;
+    int time;
     public Airport(int end, int cost, int time){
         this.end = end;
         this.cost = cost;
         this.time = time;
+    }
+
+    @Override
+    public int compareTo(Airport o){
+        return time-o.time;
     }
 }
 
@@ -17,8 +24,10 @@ public class 최단경로_10217 {
     public static int N, M, K;
     public static ArrayList<Airport>[] graph;
     public static boolean visited[];
-    public static long[] dist;
+    public static int[][] dp;
+    public static int answer = Integer.MAX_VALUE;
     public static int INF = Integer.MAX_VALUE;
+    public static StringBuilder sb = new StringBuilder();
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
@@ -40,20 +49,51 @@ public class 최단경로_10217 {
                 int d = Integer.parseInt(st.nextToken());
                 graph[u].add(new Airport(v, c, d));
             }
+
+            Dynamic();
+            if(answer==INF) sb.append("Poor KCM\n");
+            else sb.append(answer+"\n");
         }
+        System.out.println(sb);
     }
 
     public static void Dynamic(){
-        Queue<Airport> queue = new Queue<Airport>();
+        PriorityQueue<Airport> queue = new PriorityQueue<>();
         visited = new boolean[N+1];
-        dist = new long[N+1];
-        Arrays.fill(dist, INF);
-        dist[1] = 0;
+        dp = new int[N+1][M+1];
+        for(int i=1;i<=N;i++){
+            Arrays.fill(dp[i], INF);
+        }
+        dp[1][0] = 0;
         queue.add(new Airport(1, 0, 0));
+        answer = Integer.MAX_VALUE;
 
         while(!queue.isEmpty()){
-            int current_Airport = queue.poll();
-            int u = 
+            Airport current_Airport = queue.poll();
+            int u = current_Airport.end;
+            int prev_c = current_Airport.cost;
+            int prev_t = current_Airport.time;
+
+            if(u==N){
+                answer = prev_t;
+                return;
+            }
+
+            if(visited[u]) continue;
+            visited[u] = true;
+
+            for(Airport airport : graph[u]){
+                int v = airport.end;
+                int c = airport.cost;
+                int t = airport.time;
+
+                if(prev_c+c > M) continue;
+
+                if(dp[v][prev_c+c]>prev_t+t){
+                    dp[v][prev_c+c] = prev_t+t;
+                    queue.add(new Airport(v, prev_c+c, dp[v][prev_c+c]));
+                }
+            }
         }
         
     }
