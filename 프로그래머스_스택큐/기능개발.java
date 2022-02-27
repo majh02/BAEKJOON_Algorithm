04fb9c073ed5fd08ff519cd9fd31f2e26701d1cf
MAJIHYE <majh0294@gmail.com>
2022-02-27 18:53:57 +0900

    [2022-02-27]

---
+++ 프로그래머스_스택큐/기능개발.java
@@ -0,0 +1,60 @@
+package 프로그래머스_스택큐;
+
+import java.io.*;
+import java.util.*;
+
+public class 기능개발 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int progresses[] = new int[3];
+        int speeds[] = new int[3];
+        for(int i=0;i<progresses.length;i++){
+            progresses[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i=0;i<speeds.length;i++){
+            speeds[i] = Integer.parseInt(br.readLine());
+        }
+
+        int result[] = new int[3];
+        result = solution(progresses,speeds);
+
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        int size = progresses.length;
+        int[] tmp = new int[size];
+        int n = 0;
+        int num = 0, prev_num = 0;
+        
+        while(true){
+            if(num==size) break;
+            for(int i=num;i<size;i++){
+                int prgs = progresses[i];
+                int spd = speeds[i];
+
+                if(prgs>=100){
+                    if(i==0 || (i>0 && num>0)){
+                        num++;
+                    }
+                    else progresses[i] += spd;
+                }
+                else progresses[i] += spd;
+            }
+            if(num>prev_num){
+                tmp[n] = num-prev_num;
+                prev_num = num;
+                n++;
+            }
+        }
+
+        System.out.println("n: "+n);
+
+        answer = new int[n];
+        for(int i=0;i<n;i++){
+            answer[i] = tmp[i];
+        }
+        System.out.println(Arrays.toString(answer));
+        return answer;
+    }
+}
