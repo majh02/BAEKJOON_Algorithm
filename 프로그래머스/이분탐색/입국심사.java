ca7108884d458960f526f42435f01c35f9ea7d73
MAJIHYE <majh0294@gmail.com>
2022-03-08 00:09:42 +0900

    [2022-03-08]

---
+++ 프로그래머스/이분탐색/입국심사.java
@@ -0,0 +1,16 @@
+//이분탐색_1번_입국심사
+import java.util.*;
+
+public class 입국심사 {
+    public static void main(String args[]){
+        int n = 6;
+        int times = {7, 10};
+        System.out.println(solution(n, times));
+    }
+
+    public static long solution(int n, int[] times) {
+        long answer = 0;
+        return answer;
+    }
+    
+}
