480f98cf2f76ecac7e324e25364296d25f46fa84
MAJIHYE <majh0294@gmail.com>
2022-03-08 17:52:31 +0900

    [2022-03-08]

--- 프로그래머스/완전탐색/소수찾기.java
+++ 프로그래머스/완전탐색/소수찾기.java
@@ -3,22 +3,50 @@ import java.util.*;
 
 public class 소수찾기 {
     public static void main(String args[]){
-        String numbers = "17";
+        String numbers = "011";
         System.out.println(solution(numbers));
     }
     public static int solution(String numbers) {
         int answer = 0;
-        ArrayList<Integer> number = new ArrayList<>();
+        String tmp = "";
         for(int i=0;i<numbers.length();i++){
-            String num = numbers.substring(i, i+1);
-            number.add(Integer.parseInt(num));
+            DFS(numbers,tmp,i+1);
         }
-
-        for(int i=1;i<numbers.length();i++){
-            
+        for(int i=0;i<arr.size();i++){
+            answer += isPrime(arr.get(i));
         }
-
         
         return answer;
+    }
+
+    public static boolean[] visited = new boolean[10];
+    public static ArrayList<Integer> arr = new ArrayList<>();
+    public static void DFS(String str, String tmp, int m){
+        if(tmp.length()==m){
+            int num = Integer.parseInt(tmp);
+            if(!arr.contains(num)){
+                arr.add(num);
+            }
+            return;
+        }
+        else{
+            for(int i=0;i<str.length();i++){
+                if(!visited[i]){
+                    visited[i] = true;
+                    tmp+=str.charAt(i);
+                    DFS(str,tmp,m);
+                    visited[i] = false;
+                    tmp = tmp.substring(0, tmp.length()-1);
+                }
+            }
+        }
+    }
+
+    public static int isPrime(int n){
+        if(n==0 || n==1) return 0;
+        for(int i=2;i<n;i++){
+            if(n%i==0) return 0;
+        }
+        return 1;
     }
 }
