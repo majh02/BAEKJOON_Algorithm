8b0b67d02fa1fbaa1e7e7219d364c9cbe31ff4da
MAJIHYE <majh0294@gmail.com>
2022-03-11 11:34:48 +0900

    [2022-03-11]

---
+++ 프로그래머스/힙/더맵게.java
@@ -0,0 +1,31 @@
+//힙_1번_더 맵게
+import java.util.*;
+
+public class 더맵게 {
+    public static void main(String args[]){
+        int[] scoville = {1, 2, 3, 9, 10, 12};
+        int K = 7;
+        System.out.println(solution(scoville, K));
+    }
+
+    public static int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int scov : scoville){
+            queue.add(scov);
+        }
+
+        while(queue.peek()<K){
+            if(queue.size()<2){
+                answer=-1;
+                break;
+            }
+            int first = queue.poll();
+            int second = queue.poll();
+            queue.add(first+second*2);
+            answer++;
+        }
+        return answer;
+    }
+    
+}
