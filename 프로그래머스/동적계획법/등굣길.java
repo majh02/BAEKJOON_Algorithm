29e42b95babd8bb7287cac739f7086e22b1ab376
MAJIHYE <majh0294@gmail.com>
2022-03-23 17:55:42 +0900

    [2022-03-23]

--- 프로그래머스/동적계획법/등굣길.java
+++ 프로그래머스/동적계획법/등굣길.java
@@ -13,29 +13,38 @@ public class 등굣길 {
     }
 
     public static int div = 1000000007;
-    public static int[] dx = {1,0};
-    public static int[] dy = {0,1};
-    public static int[][] dp;
-    public static int[][] dist;
+    public static int max = Integer.MAX_VALUE;
+    public static long[][] dp;
+    public static long[][] dist;
     public static int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        dp = new int[n][m];
+        dp = new long[n+1][m+1];
+        for(int[] pud : puddles){
+            dp[pud[1]][pud[0]] = max;
+        }
+        dist = new long[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dist[i], max);
+        }
 
-        dp[0][0] = 1;
-        dist[0][0] = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        dp[1][1] = 1;
+        dist[1][1] = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
                 if(dp[i][j]!=0) continue;
-                dist[i][j] = Math.min(dp[i][j-1], dp[i-1][j])+1;
-                for(int[] pud : puddles){
-                    if(i==pud[0] && j==pud[1]) continue;
+                
+                dist[i][j] = Math.min(dist[i][j-1], dist[i-1][j])+1;
+                if(dist[i][j]==dist[i][j-1]+1 && dp[i][j-1]!=max){
+                    dp[i][j]+=dp[i][j-1];
                 }
-                for(int k=0;k<2;k++){
-                    
+                if(dist[i][j]==dist[i-1][j]+1 && dp[i-1][j]!=max){
+                    dp[i][j]+=dp[i-1][j];
                 }
+                if(dp[i][j]>div) dp[i][j]%=div;
             }
         }
 
+        answer = (int)(dp[n][m]%div);
         return answer;
     }
     
