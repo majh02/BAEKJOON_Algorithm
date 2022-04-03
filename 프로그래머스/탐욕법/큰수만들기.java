0e76b5468390f41a653e3fee15c160b63289a4d2
MAJIHYE <majh0294@gmail.com>
2022-04-04 01:05:29 +0900

    [2022-04-04]

--- 프로그래머스/탐욕법/큰수만들기.java
+++ 프로그래머스/탐욕법/큰수만들기.java
@@ -2,38 +2,56 @@
 
 package 프로그래머스.탐욕법;
 
+import java.util.Arrays;
+
 public class 큰수만들기 {
     public static void main(String args[]){
-        String number = "4177252841";
-        int k = 2;
+        String number = "9924";
+        int k = 1;
         System.out.println(solution(number, k));
 
     }
 
     public static String solution(String number, int k) {
         String answer = "";
-        int[] numbers = new int[number.length()];
-        int max_num = 0, max_index = 0;
-        for(int i=0;i<number.length();i++){
-            numbers[i] = number.charAt(i)-'0';
-            if(max_num<numbers[i] && i<=number.length()-k){
-                max_num = numbers[i];
-                max_index = i;
+        // int[] numbers = new int[number.length()];
+        // int max_num = 0, max_index = 0;
+        // for(int i=0;i<number.length();i++){
+        //     numbers[i] = number.charAt(i)-'0';
+        //     if(i>=k) continue;
+        //     else if(max_num<numbers[i]){
+        //         max_num = numbers[i];
+        //         max_index = i;
+        //     }
+        // }
+
+        // int anti_k = number.length()-k-1;
+        // int result = max_num;
+        // while(anti_k>0){
+        //     int prev_max_index = max_index;
+        //     max_index = find_max(numbers, anti_k, prev_max_index);
+        //     max_num = numbers[max_index];
+        //     result*=10;
+        //     result+=max_num;
+        //     anti_k--;
+        // }
+
+        // answer = Integer.toString(result);
+
+        for(int i=0;i<k;i++){
+            String max = "";
+            for(int j=0;j<number.length();j++){
+                String tmp = number.substring(0, j)+number.substring(j+1, number.length());
+                if(j==0) max = tmp;
+                else if(Long.parseLong(max) < Long.parseLong(tmp)){
+                    max = tmp;
+                }
             }
+            number = max;
+            System.out.println(number);
         }
 
-        k = number.length()-k-1;
-        int result = max_num;
-        int prev_max_index = max_index;
-        while(k>0){
-            max_index = find_max(numbers, k, prev_max_index);
-            max_num = numbers[max_index];
-            result*=10;
-            result+=max_num;
-            k--;
-        }
-
-        answer = Integer.toString(result);
+        answer = number;
         
         return answer;
     }
