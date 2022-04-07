d25bdbdcd8ea8aadadae95608d45dfa7a7736ee9
MAJIHYE <majh0294@gmail.com>
2022-04-07 14:27:18 +0900

    [2022-04-07]

--- 프로그래머스/탐욕법/단속카메라.java
+++ 프로그래머스/탐욕법/단속카메라.java
@@ -2,14 +2,21 @@
 
 package 프로그래머스.탐욕법;
 
+import java.util.*;
+
 public class 단속카메라 {
     public static void main(String args[]){
         int[][] routes = {{-20,-15}, {-14,-5}, {-18,-13}, {-5,-3}};
+        // int[][] routes = {{15,20}, {5,14}, {13,18}, {13,15},{-20,5}};
         System.out.println(solution(routes));
     }
     public static int solution(int[][] routes) {
         int answer = 0;
-
+        Arrays.sort(routes,(o1,o2)->{
+            if(o1[0]==o2[0]) return o1[1]-o2[1];
+            return o1[0]-o2[0];
+        });
+        
         int count = 0;
         int min[] = new int[routes.length];
         int max[] = new int[routes.length];
@@ -23,11 +30,11 @@ public class 단속카메라 {
             else{
                 boolean flag = false;
                 for(int cnt = 0; cnt<=count;cnt++){
-                    if(x-min[cnt]>0) {
+                    if(x-min[cnt]>=0 && x-max[cnt]<=0) {
                         min[cnt] = x;
                         flag = true;
                     }
-                    if(y-max[cnt]<0) {
+                    if(y-max[cnt]<=0 && y-min[cnt]>=0) {
                         max[cnt] = y;
                         flag = true;
                     }
