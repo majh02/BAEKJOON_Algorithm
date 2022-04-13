4a341fe384cc3003209f1cf5abb36ac53e8c9dae
MAJIHYE <majh0294@gmail.com>
2022-04-13 17:00:32 +0900

    [2022-04-13]

--- 삼성SW역량테스트기출문제/테트로미노.java
+++ 삼성SW역량테스트기출문제/테트로미노.java
@@ -12,25 +12,21 @@ public class 테트로미노 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int max_value = 0;
         int[][] paper = new int[N][M];
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine(), " ");
             for(int j=0;j<M;j++){
                 paper[i][j] = Integer.parseInt(st.nextToken());
-                max_value = Math.max(max_value, paper[i][j]);
             }
         }
 
+        boolean[][] visited = new boolean[N][M];
         for(int i=0;i<N;i++){
             for(int j=0;j<M;j++){
-                if(paper[i][j]!=max_value) continue;
-                boolean[][] visited = new boolean[N][M];
                 visited[i][j] = true;
                 find_max(N, M, paper, 1, visited, i, j, paper[i][j]);
-                if(max == max_value*4) break;
+                visited[i][j] = false;
             }
-            if(max == max_value*4) break;
         }
 
         System.out.println(max);
@@ -51,20 +47,20 @@ public class 테트로미노 {
             if(nx<0 || ny<0 || nx>=N || ny>=M) continue;
             if(visited[nx][ny]) continue;
 
+            boolean flag = false;
             if(i>=4 && count==3){
-                boolean flag = false;
+                if(sum+paper[nx][ny]<max) continue;
                 for(int j=0;j<4;j++){
                     int neihbor_x = nx+dx[j];
                     int neihbor_y = ny+dy[j];
                     if(neihbor_x<0 || neihbor_y<0 || neihbor_x>=N || neihbor_y>=M) continue;
-                    // System.out.println("nx: "+nx+", ny: "+ny+", n_x: "+neihbor_x+", n_y: "+neihbor_y);
                     if(visited[neihbor_x][neihbor_y]){
                         flag = true;
                         break;
                     }
                 }
-                if(!flag) continue;
             }
+            if(i>=4 && !flag) continue;
             
             visited[nx][ny] = true;
             find_max(N, M, paper, count+1, visited, nx, ny, sum+paper[nx][ny]);
