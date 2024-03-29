124308905b51c5681cfa1b6c6b6773aa50efdcce
마지혜 (AICC기술개발팀) <jihye.ma@kt.com>
2024-03-29 15:00:43 +0900

    2024.03.29 커밋

---
+++ 프로그래머스/_코딩올림피아드_대비/정렬/가장 큰 수.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = ''
+
+    numbers = list(map(str, numbers))
+    numbers.sort()
+    print(numbers)
+
+    # for i in range(len(numbers)):
+        
+    return answer
+
+print(solution([6, 10, 2]))
